8495acac4b3c17c281e14a53014ce86c32d89a5a
Ivan Molodetskikh <yalterz@gmail.com>
2017-10-09 10:47:28 +0300

    Laptop stuff

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -10,6 +10,10 @@ alias g "git"
 set -x PATH ~/.cargo/bin $PATH
 set -x BASE16_THEME ocean
 
+# Initialize the gnome-keyring-daemon
+set -l gnome_keyring_vars (gnome-keyring-daemon --start)
+eval (echo $gnome_keyring_vars | sed -e 's/^\(.*\)$/set -x \\1;/' -e 's/=/ /')
+
 # Vi key bindings
 fish_vi_key_bindings
 
