c4454ce671afa8597c4065962d8b03ad9cf5d49b
Ivan Molodetskikh <yalterz@gmail.com>
2017-10-19 23:23:44 +0300

    Stuff

--- common/Source/sh/osu.sh
+++ common/Source/sh/osu.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/bash
+#!/usr/bin/fish
 
-WINEPREFIX=~/.wine_osu WINEARCH=win32 wine /mnt/hdd/Games/osu\!/osu\!.exe
+set -x WINEPREFIX $HOME/.wine_osu
+set -x WINEARCH win32
+
+if math (count $argv) ">= 1" > /dev/null
+	if [ $argv[1] = "k" ]
+		exec wineserver -k
+	else
+		exec wine '/mnt/hdd/Source/osu! stuff/osufx/osu!win/bin/x86/PublicNoUpdate/osu!.exe'
+	end
+else
+	exec wine '/mnt/hdd/Games/osu!/osu!.exe'
+end
