42f51f21638e627fdfeb1a4b61235a2095b779d0
Ivan Molodetskikh <yalterz@gmail.com>
2018-01-15 10:44:13 +0300

    Add background to the taglist

--- desktop/.config/awesome/taglist.lua
+++ desktop/.config/awesome/taglist.lua
@@ -1,6 +1,7 @@
-local awful = require("awful")
-local gears = require("gears")
-local wibox = require("wibox")
+local awful     = require("awful")
+local beautiful = require("beautiful")
+local gears     = require("gears")
+local wibox     = require("wibox")
 
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
@@ -28,11 +29,11 @@ local function taglist_update(w, buttons, label, data, objects)
     w:reset()
     for i, o in ipairs(objects) do
         local cache = data[o]
-        local tb, bgb, v
+        local tb, bgb, bgc
         if cache then
             tb = cache.tb
             bgb = cache.bgb
-            v = cache.v
+            bgc = cache.bgc
         else
             tb = wibox.widget.textbox()
             tb:set_align("center")
@@ -42,20 +43,22 @@ local function taglist_update(w, buttons, label, data, objects)
 
             local bar_l = wibox.layout.fixed.horizontal()
             bar_l:add(wibox.container.constraint(nil, "exact", dpi(2)))
-            bar_l:add(bgb)
+            bar_l:add(wibox.container.place(bgb))
             local m = wibox.container.mirror(bar_l, { horizontal = true })
 
             local l = wibox.layout.stack()
             l:add(m)
             l:add(tb)
 
-            v = vertical_constraint(l, 20)
-            v:buttons(awful.widget.common.create_buttons(buttons, o))
+            local v = vertical_constraint(l, 20)
+
+            bgc = wibox.container.background(v)
+            bgc:buttons(awful.widget.common.create_buttons(buttons, o))
 
             data[o] = {
                 tb  = tb,
                 bgb = bgb,
-                v = v,
+                bgc = bgc,
             }
         end
 
@@ -63,6 +66,12 @@ local function taglist_update(w, buttons, label, data, objects)
         args = args or {}
 
         bgb:set_bg(bg)
+
+        if bg == beautiful.taglist_bg_focus then
+          bgc:set_bg("#ffffff11")
+        else
+          bgc:set_bg("#ffffff00")
+        end
 
         -- The text might be invalid, so use pcall.
         if text == nil or text == "" then
@@ -72,7 +81,7 @@ local function taglist_update(w, buttons, label, data, objects)
             end
         end
 
-        w:add(v)
+        w:add(bgc)
    end
 end
 
