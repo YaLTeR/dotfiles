24783475591c11bed292c3338145ad9576236493
Ivan Molodetskikh <yalterz@gmail.com>
2020-04-08 21:29:59 +0300

    Update runhl-bxt.sh

--- common/source/sh/runhl-bxt.sh
+++ common/source/sh/runhl-bxt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export HL_ROOT="$HOME/Half-Life"
-export LD_LIBRARY_PATH=$HL_ROOT
-export LD_PRELOAD=/usr/lib32/libSDL2.so:~/source/cpp/BunnymodXT/build/libBunnymodXT.so
+export LD_PRELOAD=~/source/cpp/BunnymodXT/build/libBunnymodXT.so
 if [ -n "$PRELOAD" ]; then
 	export LD_PRELOAD=$PRELOAD:$LD_PRELOAD
 fi
 
 cd $HL_ROOT
-exec ./hl_linux -steam "$@"
-# exec gdb ./hl_linux
+# exec perf record --call-graph dwarf,65528 -F 100 -D 5000 ./hl_linux -steam "$@"
+# exec ~/.steam/bin/steam-runtime/run.sh ./hl_linux -steam "$@"
+exec ~/.steam/bin/steam-runtime/run.sh gdb ./hl_linux
