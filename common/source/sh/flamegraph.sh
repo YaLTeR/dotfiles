5c64f93a5b8fc5ddb1056cde78f08d42121c2976
Ivan Molodetskikh <yalterz@gmail.com>
2020-08-05 20:56:46 +0300

    sh/flamegraph: update paths

--- common/source/sh/flamegraph.sh
+++ common/source/sh/flamegraph.sh
@@ -13,10 +13,12 @@ main () {
 	perf script -i "$1" > "$BASENAME.perf"
 
 	echo "Launching stackcollapse-perf.pl..."
-	~/Source/pl/FlameGraph/stackcollapse-perf.pl "$BASENAME.perf" > "$BASENAME.folded"
+	# ~/source/pl/FlameGraph/stackcollapse-perf.pl "$BASENAME.perf" > "$BASENAME.folded"
+	~/source/rs/inferno/target/release/inferno-collapse-perf "$BASENAME.perf" > "$BASENAME.folded"
 
 	echo "Launching flamegraph.pl..."
-	~/Source/pl/FlameGraph/flamegraph.pl "$BASENAME.folded" > "$BASENAME.svg"
+	# ~/source/pl/FlameGraph/flamegraph.pl "$BASENAME.folded" > "$BASENAME.svg"
+	~/source/rs/inferno/target/release/inferno-flamegraph "$BASENAME.folded" > "$BASENAME.svg"
 
 	echo "Launching Firefox..."
 	firefox "$BASENAME.svg"
