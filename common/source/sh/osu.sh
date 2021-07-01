16e7f462e190f2d7cc328fe3c96d22c9812c310c
Ivan Molodetskikh <yalterz@gmail.com>
2021-07-01 08:00:42 +0300

    sh/osu: Tweak for pipewire

--- common/source/sh/osu.sh
+++ common/source/sh/osu.sh
@@ -2,7 +2,11 @@
 
 export WINEPREFIX=$HOME/.wine_osu
 export WINEARCH=win32
-export STAGING_AUDIO_DURATION=80000
+# export STAGING_AUDIO_DURATION=80000
+# export PULSE_LATENCY_MSEC=14
+
+# Doesn't help the latency but somehow makes the playfield smoother.
+export PIPEWIRE_LATENCY=64/44100
 
 if [ $# -ge 1 ]
 then
