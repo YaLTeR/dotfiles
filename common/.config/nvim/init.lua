c364cbab1c736695ba3c32990c06755bc6df5e27
Ivan Molodetskikh <yalterz@gmail.com>
2024-01-15 08:07:28 +0400

    nvim: Use z for surround binds (for leap)

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -84,7 +84,21 @@ require('lazy').setup({
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
     config = function()
-        require('nvim-surround').setup {}
+        require('nvim-surround').setup {
+          keymaps = {
+            insert = "<C-g>z",
+            insert_line = "<C-g>Z",
+            normal = "yz",
+            normal_cur = "yzz",
+            normal_line = "yZ",
+            normal_cur_line = "yZZ",
+            visual = "Z",
+            visual_line = "gZ",
+            delete = "dz",
+            change = "cz",
+            change_line = "cZ",
+          },
+        }
     end,
   },
 
