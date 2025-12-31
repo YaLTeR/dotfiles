e701bcda034a40d4f8a594ef5f96fc40a14add9e
Ivan Molodetskikh <yalterz@gmail.com>
2025-12-31 13:54:45 +0300

    Add libinput plugin for G502 X hi-res scroll

---
+++ dot_config/libinput/plugins/10-g502x-hires.lua
@@ -0,0 +1,14 @@
+libinput:register({1})
+libinput:connect("new-evdev-device", function (device)
+    local info = device:info()
+    if info.vid == 0x046D and info.pid == 0xC099 then
+        device:connect("evdev-frame", function (device, frame, timestamp)
+            for _, event in ipairs(frame) do
+                if event.usage == evdev.REL_WHEEL_HI_RES then
+                    event.value = event.value / 8
+                end
+            end
+            return frame
+        end)
+    end
+end)
