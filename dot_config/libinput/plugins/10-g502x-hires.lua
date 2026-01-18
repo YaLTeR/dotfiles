22044894067371d5b795736e7d1787399a2f307b
Ivan Molodetskikh <yalterz@gmail.com>
2026-01-18 21:04:59 +0300

    libinput plugin: Disable wheel debouncing
    Will work in the next libinput release.

--- dot_config/libinput/plugins/10-g502x-hires.lua
+++ dot_config/libinput/plugins/10-g502x-hires.lua
@@ -2,6 +2,7 @@ libinput:register({1})
 libinput:connect("new-evdev-device", function (device)
     local info = device:info()
     if info.vid == 0x046D and info.pid == 0xC099 then
+        device:disable_feature("wheel-debouncing")
         device:connect("evdev-frame", function (device, frame, timestamp)
             for _, event in ipairs(frame) do
                 if event.usage == evdev.REL_WHEEL_HI_RES then
