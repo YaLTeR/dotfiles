edbbc16e79f6ad1fa6cb107f12156f6669ef6f98
Ivan Molodetskikh <yalterz@gmail.com>
2026-08-05 17:29:43 +0300

    nvim: Update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -181,17 +181,7 @@ require('lazy').setup {
     event = 'VeryLazy',
   },
 
-  {
-    'https://codeberg.org/andyg/leap.nvim',
-    opts = {
-      -- Exclude whitespace and the middle of alphabetic words from preview:
-      --   foobar[baaz] = quux
-      --   ^----^^^--^^-^-^--^
-      preview = function(ch0, ch1, ch2)
-        return not (ch1:match('%s') or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a')))
-      end,
-    },
-  },
+  'https://codeberg.org/andyg/leap.nvim',
 
   {
     'nvim-telescope/telescope.nvim',
